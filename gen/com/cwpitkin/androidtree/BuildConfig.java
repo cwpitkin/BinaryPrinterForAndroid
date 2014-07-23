6b1a9119bb457a5cf444969c6553b04ffa3d93a0
Chris Pitkin <cwpitkin@gmail.com>
2014-07-23 01:18:15 -0700

    adding android project files

---
+++ gen/com/cwpitkin/androidtree/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cwpitkin.androidtree;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/cwpitkin/androidtree/BinarySearchTreeIteratorException.java
@@ -0,0 +1,20 @@
+package com.cwpitkin.androidtree;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author C0255969
+ */
+public class BinarySearchTreeIteratorException extends RuntimeException{
+
+    public BinarySearchTreeIteratorException() {
+        super();
+    }
+
+    public BinarySearchTreeIteratorException(String s) {
+        super(s);
+    }
+}
